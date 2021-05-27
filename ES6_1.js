a211feaebf0081816f58109622d070ddb9dd9135
Rupesh Gelal <rupeshgelal9@gmail.com>
2021-05-27 17:38:30 +0545

    var,let,and const

---
+++ ES6_1.js
@@ -0,0 +1,27 @@
+function myFunc(){
+    for (var i=0; i<5;i++){ // var scoped within function
+        console.log(i);
+    }
+    console.log(i+5);
+}
+
+myFunc();
+
+function myFunc(){
+    for (let i=0; i<5;i++){ // let scoped within block
+        console.log(i);
+    }
+    console.log(i+5); // throws error here
+}
+
+myFunc();
+
+//const is also scoped within block
+
+let x =1;
+x=2;
+console.log(x); // no error as value of let can be changed
+
+const val = 2;
+val = 4;
+console.log(val); // throws error, variable declared with const can't be reassigned 
