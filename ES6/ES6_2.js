4c05f68863e45e2b8c1a705e752e93fe0d9f86c8
Rupesh Gelal <rupeshgelal9@gmail.com>
2021-05-29 17:28:32 +0545

    obj. in ES6

---
+++ ES6/ES6_2.js
@@ -0,0 +1,13 @@
+const person = { //obj defined; key value pair
+    name : '',
+    walk : function(){},
+    talk(){} // method inside obj in ES6
+};
+
+person.walk(); // method called
+person['name'] = 'Ted';
+
+const targetValue = 'name'; // used dynamically, say for input field
+person['name'] = 'Hola';
+
+console.log(person.name);
--- ES6/tempCodeRunnerFile.js
+++ ES6/tempCodeRunnerFile.js
@@ -1,19 +1,2 @@
-
-// function myFunc(){
-//     for (let i=0; i<5;i++){ // let scoped within block
-//         console.log(i);
-//     }
-//     console.log(i+5); // throws error here
-// }
-
-// myFunc();
-
-// //const is also scoped within block
-
-// let x =1;
-// x=2;
-// console.log(x); // no error as value of let can be changed
-
-// const val = 2;
-// val = 4;
-// console.log(val); // throws error, variable declared with const can't be reassigned 
+person.walk(); // method called
+person['name'] = 'Ted';
